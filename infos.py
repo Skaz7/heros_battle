80d9bdd33be8ac3c2abb66b5a5a9525a39751ba7
Sebastian Kupis <sebastian.kupis@gmail.com>
2023-08-16 22:50:47 +0200

    Battle info rebuilt

--- infos.py
+++ infos.py
@@ -12,44 +12,71 @@ def print_basic_stats(creature):
 
 def print_battle_stats(player, enemy):
     print()
-    print((player.name).ljust(30) + "<----->" + (enemy.name).rjust(30))
+    print((player.name).ljust(30) + "<---->" + (enemy.name).rjust(30))
     print("-" * 67)
     print(
-        f"Health     : {player.health}/{player.max_health}".ljust(30)
-        + "       "
-        + f"Health     : {enemy.health}/{enemy.max_health}".rjust(30)
+        f"{player.health}/{player.max_health}".ljust(25)
+        + "<-   HEALTH   ->"
+        + f"{enemy.health}/{enemy.max_health}".rjust(25)
     )
     print(
-        f"Level      : {player.level}".ljust(30)
-        + "       "
-        + f"Level      :     {enemy.level}".rjust(30)
+        f"{player.level}".ljust(25)
+        + "<-   LEVEL    ->"
+        + f"{enemy.level}".rjust(25)
     )
     print(
-        f"Experience : {player.experience:}".ljust(30)
-        + "       "
-        + f"Experience :    {enemy.experience}".rjust(30)
+        f"{player.experience:}".ljust(25)
+        + "<- EXPERIENCE ->"
+        + f"{enemy.experience}".rjust(25)
     )
-    print(f"Mana       : {player.mana}/{player.max_mana}".ljust(30))
+    print(f"{player.mana}/{player.max_mana}".ljust(25) + "<-    MANA    ->")
     print(
-        f"Status     : {', '.join(status.name for status in player.statuses)}".ljust(30)
-        + "       "
-        + f"Status     :    {', '.join(status.name for status in enemy.statuses)}".rjust(30)
+            f"{', '.join(status.name for status in player.statuses)}".ljust(25)
+        + "<-   STATUS   ->"
+        + f"{', '.join(status.name for status in enemy.statuses)}".rjust(25)
     )
 
-    try:
-        print(f"Weapon    -> {player.equipped_weapon.name}")
-    except AttributeError:
-        print("Weapon    -> None")
+    if player.equipped_weapon is None:
+        player_weapon = None
+    else:
+        player_weapon = player.equipped_weapon.name
+    if enemy.equipped_weapon is None:
+        enemy_weapon = None
+    else:
+        enemy_weapon = enemy.equipped_weapon.name
+    print(
+        f"{player_weapon}".ljust(25)
+        + "<-   WEAPON   ->"
+        + f"{enemy_weapon}".rjust(25)
+    )
 
-    try:
-        print(f"Shield    -> {player.equipped_shield.name}")
-    except AttributeError:
-        print("Shield    -> None")
+    if player.equipped_shield is None:
+        player_shield = None
+    else:
+        player_shield = player.equipped_shield.name
+    if enemy.equipped_shield is None:
+        enemy_shield = None
+    else:
+        enemy_shield = enemy.equipped_shield.name
+    print(
+        f"{player_shield}".ljust(25)
+        + "<-   SHIELD   ->"
+        + f"{enemy_shield}".rjust(25)
+    )
 
-    try:
-        print(f"Armor     -> {player.equipped_armor.name}")
-    except AttributeError:
-        print("Armor     -> None")
+    if player.equipped_armor is None:
+        player_armor = None
+    else:
+        player_armor = player.equipped_armor.name
+    if enemy.equipped_armor is None:
+        enemy_armor = None
+    else:
+        enemy_armor = enemy.equipped_armor.name
+    print(
+        f"{player_armor}".ljust(25)
+        + "<-   ARMOR    ->"
+        + f"{enemy_armor}".rjust(25)
+    )
 
     print("-" * 67)
     print()
